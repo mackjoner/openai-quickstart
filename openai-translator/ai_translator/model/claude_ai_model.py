19bf95a05d87bd3dca8bb8432d16dc9b3411556c
johnny <mackjoner@gmail.com>
2024-03-23 22:30:52 +0800

    feat: #000 add ai translator for web ui and api

--- openai-translator/ai_translator/model/claude_ai_model.py
+++ openai-translator/ai_translator/model/claude_ai_model.py
@@ -1,48 +1,40 @@
 import time
-import openai
+import anthropic
 
 from model import Model
 from utils import LOG
-from openai import OpenAI
+from anthropic import Anthropic
 
-class OpenAIModel(Model):
+class ClaudeAIModel(Model):
     def __init__(self, model: str, api_key: str):
         self.model = model
-        self.client = OpenAI(api_key=api_key)
+        self.client = Anthropic(api_key=api_key)
 
     def make_request(self, prompt):
         attempts = 0
         while attempts < 3:
             try:
-                if self.model == "gpt-3.5-turbo":
-                    response = self.client.chat.completions.create(
-                        model=self.model,
-                        messages=[
-                            {"role": "user", "content": prompt}
-                        ]
-                    )
-                    translation = response.choices[0].message.content.strip()
-                else:
-                    response = self.client.completions.create(
-                        model=self.model,
-                        prompt=prompt,
-                        max_tokens=150,
-                        temperature=0
-                    )
-                    translation = response.choices[0].text.strip()
-
+                response = self.client.messages.create(
+                    max_tokens=1024,
+                    messages=[
+                        {"role": "user", "content": prompt}
+                    ],
+                    model=self.model
+                )
+                print(response.content)
+                translation = response.content[0].text
                 return translation, True
-            except openai.RateLimitError as e:
+            except anthropic.RateLimitError as e:
                 attempts += 1
                 if attempts < 3:
                     LOG.warning("Rate limit reached. Waiting for 60 seconds before retrying.")
                     time.sleep(60)
                 else:
                     raise Exception("Rate limit reached. Maximum attempts exceeded.")
-            except openai.APIConnectionError as e:
+            except anthropic.APIConnectionError as e:
                 print("The server could not be reached")
                 print(e.__cause__)  # an underlying Exception, likely raised within httpx.            except requests.exceptions.Timeout as e:
-            except openai.APIStatusError as e:
+            except anthropic.APIStatusError as e:
                 print("Another non-200-range status code was received")
                 print(e.status_code)
                 print(e.response)
